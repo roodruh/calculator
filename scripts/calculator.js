61897f6d6ed39c10dd91a365eec5d3600cb3b438
roodruh <161749877+roodruh@users.noreply.github.com>
2024-05-19 20:00:25 +0530

    add input box

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,23 +1,48 @@
 const body = document.querySelector('body');
+const hr = document.createElement('hr');
 
+//creating app window
 const app = document.createElement('div');
 body.appendChild(app);
 app.setAttribute('id', 'app');
 
+//creating row 0 of the app window
 const row0 = document.createElement('div');
 app.appendChild(row0);
-row0.setAttribute('id', 'row0');
+row0.setAttribute('class', 'row');
 
+//adding calculator title to row 0
 const calcTitle = document.createElement('div');
 row0.appendChild(calcTitle);
 calcTitle.setAttribute('id', 'calc-title');
 calcTitle.setAttribute('class', 'title');
 calcTitle.innerHTML = 'CALCULATOR';
 
+//adding battery symbol to row 0
 const battery = document.createElement('div');
 row0.appendChild(battery);
 battery.setAttribute('id', 'battery');
 battery.innerHTML = '#####';
 
-const hr = document.createElement('hr');
+//adding hr element after row 0
 app.appendChild(hr);
+
+//creating row 1
+const row1 = document.createElement('div');
+app.appendChild(row1);
+row1.setAttribute('class', 'row');
+
+//adding form elemnt to row 1
+const calcInput = document.createElement('input');
+row1.appendChild(calcInput);
+calcInput.setAttribute('type', 'form');
+calcInput.setAttribute('id', 'calc-input');
+
+//creating row 2
+const row2 = document.createElement('div');
+app.appendChild(row2);
+row2.setAttribute('class', 'row');
+
+//creating row above numberpad of calculator
+const topRow = document.createElement('div');
+row2.appendChild('')
